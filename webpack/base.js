3ba3a411cb18df080c576db020b5bb17806f92d0
Nik Cernomorsky <nik.cernomorsky@gmail.com>
2020-09-03 12:11:59 +0200

    fix 5

---
+++ webpack/base.js
@@ -0,0 +1,41 @@
+const webpack = require("webpack");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  devtool: "eval-source-map",
+  entry: "./src/index",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          query: {
+            presets: ["@babel/react"],
+            plugins: ["@babel/proposal-class-properties"],
+          },
+        },
+      },
+      {
+        test: [/\.vert$/, /\.frag$/],
+        use: "raw-loader",
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg|xml)$/i,
+        use: "file-loader",
+      },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader",
+      },
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      CANVAS_RENDERER: JSON.stringify(true),
+      WEBGL_RENDERER: JSON.stringify(true),
+    }),
+  ],
+};
